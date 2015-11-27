Finish check gradient and fix nn_cost_function

check_nn_gradients.py
@@ -16,11 +16,9 @@ def check_nn_gradients(_lambda=0):
     theta2 = debug_initialize_weights(num_labels, hidden_layer_size)
     # Reusing debugInitializeWeights to generate X
     x = debug_initialize_weights(m, input_layer_size - 1)
+    y = np.mod(np.arange(1, m+1), num_labels).T
 
-    y = np.mod(np.arange(m), num_labels).T
-
-    nn_params = np.concatenate((theta1.ravel(), theta2.ravel()))
-
+    nn_params = np.concatenate((theta1.T.ravel(), theta2.T.ravel()))
     def cost_function(p):
         return nn_cost_function(p, input_layer_size, hidden_layer_size, num_labels, x, y, _lambda)
 

debug_initialize_weights.py
@@ -16,4 +16,4 @@ def debug_initialize_weights(fan_out, fan_in):
 
     # Initialize W using "sin", this ensures that W is always of the same
     # values and will be useful for debugging
-    return np.reshape(np.sin(np.arange(length)), (fan_out, fan_in + 1)) / 10
+    return np.reshape(np.sin(np.arange(1, length+1)), (fan_out, fan_in + 1), order='F') / 10

main.py
@@ -33,17 +33,24 @@ print('Tests nn_cost_function with default data')
 test_theta1 = weights['Theta1']
 test_theta2 = weights['Theta2']
 
-nn_params = np.concatenate((test_theta1.ravel(), test_theta2.ravel()))
+nn_params = np.concatenate((test_theta1.T.ravel(), test_theta2.T.ravel()))
 
 _lambda = 0
-j = nn_cost_function(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y_test, _lambda)
+j, _ = nn_cost_function(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y_test, _lambda)
 print('\nCost without reg', j)
 print('(this value should be about 0.287629) \n')
 
 _lambda = 1
-j = nn_cost_function(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y_test, _lambda)
+j, _ = nn_cost_function(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y_test, _lambda)
 print('\nReal cost', j)
 print('(this value should be about 0.383770) \n')
+
+print('\nEvaluating sigmoid gradient...\n')
+
+g = sigmoid_gradient(np.array([1, -0.5, 0, 0.5, 1]))
+print('Sigmoid gradient evaluated at [1 -0.5 0 0.5 1]:\n  ')
+print(g)
+print('\n\n')
 
 print('Check gradients')
 check_nn_gradients()

nn_cost_function.py
@@ -6,9 +6,9 @@ from sigmoid import sigmoid, sigmoid_gradient
 def nn_cost_function(nn_params, input_layer_size, hidden_layer_size, num_labels, x, y, _lambda):
 
     theta1 = np.reshape(nn_params[:hidden_layer_size * (input_layer_size + 1)],
-                        (hidden_layer_size, (input_layer_size + 1)))
+                        (hidden_layer_size, (input_layer_size + 1)), order='F')
     theta2 = np.reshape(nn_params[(hidden_layer_size * (input_layer_size + 1)):],
-                        (num_labels, (hidden_layer_size + 1)))
+                        (num_labels, (hidden_layer_size + 1)), order='F')
 
     m = x.shape[0]
 
@@ -35,22 +35,22 @@ def nn_cost_function(nn_params, input_layer_size, hidden_layer_size, num_labels,
     # Backward propagation to compute gradients
     delta3 = (a3 - y.T)
     delta2 = np.dot(theta2[:, 1:].T, delta3) * sigmoid_gradient(z2)
-    delta1 = np.dot(delta2, a1.T)
-    delta2 = np.dot(delta3, a2.T)
+    _Delta1 = np.dot(delta2, a1.T)
+    _Delta2 = np.dot(delta3, a2.T)
 
-    theta1_grad = (1/m) * delta1
-    theta2_grad = (1/m) * delta2
+    theta1_grad = (1/m) * _Delta1
+    theta2_grad = (1/m) * _Delta2
 
     # Regularize gradients
-    temp1 = theta1
+    temp1 = np.copy(theta1)
     temp1[:, 0] = 0
     theta1_grad += np.dot((_lambda/m),  temp1)
 
-    temp2 = theta2
+    temp2 = np.copy(theta2)
     temp2[:, 0] = 0
     theta2_grad += np.dot((_lambda/m), temp2)
 
-    gradients = np.concatenate((theta1_grad.ravel(), theta2_grad.ravel()))
+    gradients = np.concatenate((theta1_grad.T.ravel(), theta2_grad.T.ravel()))
 
     return j, gradients
 

sigmoid.py
@@ -1,10 +1,9 @@
 
 import numpy as np
 
-
 def sigmoid(z):
     return np.true_divide(1.0, 1.0 + np.exp(-z))
 
 
 def sigmoid_gradient(z):
-    return sigmoid(z) * 1.0 - sigmoid(z)
+    return sigmoid(z) * (1.0 - sigmoid(z))